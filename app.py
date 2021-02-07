d3b69b549717fefdba91fd25574fa96e77d89609
balajisunder <balaji.sunder@gmail.com>
2021-02-07 12:44:45 -0600

    v2.1 - removed warnings, unused code, added comments.

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/input/')
+@app.route("/input.html/")
+def input_data():
+    return render_template("input.html")
+
+
 @app.route('/output/', methods=['POST', 'GET'])
 @app.route("/output.html/", methods=['POST', 'GET'])
 def results():
@@ -23,7 +29,6 @@ def results():
         # redirect to input html if output is typed directly
         return render_template("input.html")
     if request.method == 'POST':
-        # run the returns
 
         # check DOB format and update to right one, if required
         dob = request.form['date_of_birth']
@@ -37,32 +42,22 @@ def results():
         else:
             dob = dob + "-01-01"
             dob = datetime.strptime(dob, '%Y-%m-%d')
+
+        # run the returns
         current_age = relativedelta(date.today(), dob).years
         retirement_planned_age = int(request.form['retirement_age'])
-        if current_age < retirement_planned_age:  # Not yet retired, run simulation.
-            target_percentile_subset, pass_percentile, pass_percentile_value, final_year_median_value, verdict = \
-                simulate_returns(request.form['iterations'],
-                                 request.form['present_value'],
-                                 request.form['expected_roi'], request.form[
-                                     'standard_deviation_of_expected_returns'],
-                                 current_age, retirement_planned_age,
-                                 request.form[
-                                     'annual_savings_till_retirement'],
-                                 request.form[
-                                     'annual_withdrawal_in_retirement'],
-                                 request.form['max_age'])
-            chart_display, chart_div, cdn_js = cr.create_reports(target_percentile_subset)
+        target_percentile_subset, pass_percentile, pass_percentile_value, final_year_median_value, verdict = \
+            simulate_returns(request.form['iterations'],
+                             request.form['present_value'],
+                             request.form['expected_roi'],
+                             request.form['standard_deviation_of_expected_returns'],
+                             current_age,
+                             retirement_planned_age,
+                             request.form['annual_savings_till_retirement'],
+                             request.form['annual_withdrawal_in_retirement'],
+                             request.form['max_age'])
+        chart_display, chart_div, cdn_js = cr.create_reports(target_percentile_subset)
 
-        else:  # already retired?
-            pass_percentile = "N/A"
-            pass_percentile_value = "N/A"
-            final_year_median_value = "N/A"
-            target_percentile_subset = "N/A"
-            verdict = "You seem to have retired. Functionality for calculating viability for retirees is currently " \
-                      "unavailable and will be a part of our future roadmap"
-            chart_display = ""
-            chart_div = "<div></div>"
-            cdn_js = ["", "", ""]
         return render_template("output.html",
                                passing_percentile=pass_percentile,
                                passing_percentile_value=pass_percentile_value,
@@ -72,12 +67,6 @@ def results():
                                chart_to_display=chart_display,
                                div_for_chart=chart_div,
                                chart_js=cdn_js[0], chart_js_tables=cdn_js[2], chart_js_widgets=cdn_js[1])
-
-
-@app.route('/input/')
-@app.route("/input.html/")
-def input_data():
-    return render_template("input.html")
 
 
 if __name__ == '__main__':
